55f50d2aee5f7226fef33416431cf3f070ccc214
wojciechzyla <62514065+wojciechzyla@users.noreply.github.com>
2024-01-07 23:16:18 +0100

    add processing app and encrypted communication between apps in kubenretes

--- app1/app.py
+++ app1/app.py
@@ -8,6 +8,10 @@ import logging
 from functools import wraps
 from flask_sqlalchemy import SQLAlchemy
 import os
+from Crypto.Cipher import AES
+import base64
+import json
+import requests
 
 DB_NAME = os.getenv("DB_NAME", "smart_travel")
 DB_HOST = os.getenv("DB_HOST", "0.0.0.0:5432")
@@ -15,6 +19,8 @@ DB_USERNAME = os.getenv("DB_USERNAME", "st_backend")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "abdj24AfwF#$1cw#4fq3d")
 ACCESS_TOKEN_EXPIRES_MIN = int(os.getenv("ACCESS_TOKEN_EXPIRES_MIN", "2"))
 REFRESH_TOKEN_EXPIRES_MIN = int(os.getenv("REFRESH_TOKEN_EXPIRES_MIN", "6"))
+APP2_URL = os.getenv("APP2_URL")
+ENCRYPTION_KEY = bytes(os.environ.get('ENCRYPTION_KEY'), "utf-8")
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -26,8 +32,32 @@ app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(minutes=ACCESS_TOKEN_EXPIRES_
 app.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(minutes=REFRESH_TOKEN_EXPIRES_MIN)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db = SQLAlchemy(app)
+
+
+def encrypt_data(data, key):
+    header = b"header"
+    cipher = AES.new(key, AES.MODE_GCM)
+    cipher.update(header)
+    encrypted_data, tag = cipher.encrypt_and_digest(data.encode("utf-8"))
+
+    encrypted_data = base64.b64encode(encrypted_data).decode('utf-8')
+    nonce = base64.b64encode(cipher.nonce).decode('utf-8')
+    tag = base64.b64encode(tag).decode('utf-8')
+    return encrypted_data, nonce, tag
+
+
+def decrypt_data(encrypted_data, key, nonce, tag):
+    try:
+        header = b"header"
+        cipher = AES.new(key, AES.MODE_GCM, nonce=nonce)
+        cipher.update(header)
+        decrypted_data = cipher.decrypt_and_verify(encrypted_data, tag)
+        return decrypted_data
+    except Exception as e:
+        logging.error(f"Decryption error: {str(e)}")
+        return None
+
 
 
 class User(db.Model):
@@ -141,6 +171,34 @@ def get_user():
     existinguser = User.query.filter_by(id=current_user).first()
     return jsonify({'login': existinguser.login}), 200
 
+
+@app.route('/between-apps-communication', methods=['GET'])
+@log_remote_address
+@jwt_required()
+def between_apps_communication():
+    current_user = get_jwt_identity()
+    existinguser = User.query.filter_by(id=current_user).first()
+    encrypted_data, nonce, tag = encrypt_data(json.dumps({"username": existinguser.login}), ENCRYPTION_KEY)
+
+    logging.info("Sending request to processing app")
+    response = requests.post(f"http://{APP2_URL}/process", json={'data': encrypted_data, 'nonce': nonce, 'tag': tag})
+    if response.status_code != 200:
+        return jsonify({'error': 'We are facing some problems'}), 500
+
+    encrypted_data = response.json().get('data')
+    nonce = response.json().get('nonce')
+    tag = response.json().get('tag')
+    encrypted_data = base64.b64decode(encrypted_data)
+    nonce = base64.b64decode(nonce)
+    tag = base64.b64decode(tag)
+
+    decrypted_result = decrypt_data(encrypted_data, ENCRYPTION_KEY, nonce, tag)
+
+    if decrypted_result is None:
+        return jsonify({'error': 'We are facing some problems'}), 500
+
+    decrypted_result = decrypted_result.decode("utf-8")
+    return jsonify({'message': decrypted_result}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ app2/entrypoint.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gunicorn -b :5000 app:app --log-level INFO
